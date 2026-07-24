1f6f682c50d4bdd49e860dd2d6769a7af3933502
AyushSemz <warriorayush123@gmail.com>
2026-07-24 16:16:59 +0530

    .

--- Dashboard-client/src/components/Add.js
+++ Dashboard-client/src/components/Add.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBTooltip, MDBIcon } from 'mdb-react-ui-kit';
-import NoteModal from './NoteModal';
-import TodoModal from './TodoModal';
+import React, { useState } from "react";
+import {
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBTooltip,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import NoteModal from "./NoteModal";
+import TodoModal from "./TodoModal";
 
 function Add({ addNote, addTodoList }) {
   // Define state variables for showing/hiding the note and todo modals
@@ -22,14 +29,14 @@ function Add({ addNote, addTodoList }) {
     await addNote({
       title,
       content,
-      color
+      color,
     });
   }
 
   async function createTodoList(title, color) {
     await addTodoList({
       title,
-      color
+      color,
     });
   }
 
@@ -37,19 +44,81 @@ function Add({ addNote, addTodoList }) {
   return (
     <>
       {/* Render the note modal */}
-      <NoteModal key="1" modalTitle="Add New Note" modalButton="Create Note" showAddNote={showAddNote} setShowAddNote={setShowAddNote} toggleShowAddNote={toggleAddNote} createUpdateNote={createNote} />
+      <NoteModal
+        key="1"
+        modalTitle="Add New Note"
+        modalButton="Create Note"
+        showAddNote={showAddNote}
+        setShowAddNote={setShowAddNote}
+        toggleShowAddNote={toggleAddNote}
+        createUpdateNote={createNote}
+      />
 
       {/* Render the todo modal */}
-      <TodoModal key="2" modalTitle="Add New Todo List" modalButton="Create Todo List" showTodo={showAddTodoList} setShowTodo={setShowAddTodoList} toggleShowTodo={toggleAddTodoList} createUpdateTodo={createTodoList} />
+      <TodoModal
+        key="2"
+        modalTitle="Add New Todo List"
+        modalButton="Create Todo List"
+        showTodo={showAddTodoList}
+        setShowTodo={setShowAddTodoList}
+        toggleShowTodo={toggleAddTodoList}
+        createUpdateTodo={createTodoList}
+      />
 
       {/* Render the dropdown menu for adding new notes and todo lists */}
       <MDBDropdown dropleft group>
         <MDBTooltip tag="span" title={"Add New Note or To Do"}>
-          <MDBDropdownToggle color="link" > <MDBIcon fas icon='plus' size='lg' /> </MDBDropdownToggle>
+          <MDBDropdownToggle
+            color="link"
+            style={{
+              color: "#3b82f6",
+              backgroundColor: "#111111",
+              border: "1px solid #222222",
+              borderRadius: "10px",
+              padding: "0.6rem 0.9rem",
+              boxShadow: "0 4px 12px rgba(0, 0, 0, 0.4)",
+            }}
+          >
+            <MDBIcon fas icon="plus" size="lg" />
+          </MDBDropdownToggle>
         </MDBTooltip>
-        <MDBDropdownMenu>
-          <MDBDropdownItem name="note" link onClick={toggleAddNote}> <MDBIcon fas icon="sticky-note" className='mx-1'/> Note</MDBDropdownItem>
-          <MDBDropdownItem name="todo" link onClick={toggleAddTodoList}> <MDBIcon fas icon="check-square" className='mx-1' /> ToDo List</MDBDropdownItem>
+        <MDBDropdownMenu
+          style={{
+            backgroundColor: "#111111",
+            border: "1px solid #222222",
+            borderRadius: "10px",
+            overflow: "hidden",
+            minWidth: "150px",
+          }}
+        >
+          <MDBDropdownItem
+            name="note"
+            link
+            onClick={toggleAddNote}
+            style={{ color: "#ffffff", backgroundColor: "transparent" }}
+          >
+            <MDBIcon
+              fas
+              icon="sticky-note"
+              className="me-2"
+              style={{ color: "#3b82f6" }}
+            />{" "}
+            Note
+          </MDBDropdownItem>
+          <MDBDropdownItem
+            name="todo"
+            link
+            onClick={toggleAddTodoList}
+            style={{ color: "#ffffff", backgroundColor: "transparent" }}
+          >
+            <MDBIcon
+              fas
+              icon="check-square"
+              className="me-2"
+              style={{ color: "#10b981" }}
+            />{" "}
+            ToDo List
+          </MDBDropdownItem>
         </MDBDropdownMenu>
       </MDBDropdown>
     </>
